ceaf7e83f47b8b5e8abb99265090d1333588f379
snyssen <dev@snyssen.be>
2025-12-15 17:12:29 +0100

    feat: use garnix cache

--- flake.nix
+++ flake.nix
@@ -9,10 +9,16 @@
 
       # nix community's cache server
       "https://nix-community.cachix.org"
+
+      # garnix
+      "https://cache.garnix.io"
     ];
     trusted-public-keys = [
       # nix community's cache server public key
       "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+
+      # garnix
+      "cache.garnix.io:CTFPyKSLcx5RMJKfLo5EEPUObbA78b0YQ2DTCJXqr9g="
     ];
   };
 
