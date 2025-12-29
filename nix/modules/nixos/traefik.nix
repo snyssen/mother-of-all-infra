fd5e5cfecf7925acc8cdbbaf90aa1af17f828825
snyssen <dev@snyssen.be>
2025-12-29 13:08:31 +0100

    feat(nix/hosts/ingress): [WIP] test reverse proxy over app server

--- nix/modules/nixos/traefik.nix
+++ nix/modules/nixos/traefik.nix
@@ -20,6 +20,9 @@ in
           Path to API key file for DNS challenge, REQUIRED if challengeType is "dns". Should be provided using a SOPS secret.
         '';
       };
+      dnsChallenge.domains = lib.mkOption {
+        default = [ "snyssen.be" ];
+      };
     };
   };
 
@@ -45,12 +48,12 @@ in
             address = ":443";
             asDefault = true;
             http.tls.certResolver = "letsencrypt";
-            http.tls.domains = lib.mkIf (cfg.letsencrypt.challengeType == "dns") [
-              {
-                main = "ingress.snyssen.be";
-                sans = [ "*.ingress.snyssen.be" ];
-              }
-            ];
+            http.tls.domains = lib.mkIf (cfg.letsencrypt.challengeType == "dns") (
+              lib.lists.map (domain: {
+                main = "${domain}";
+                sans = [ "*.${domain}" ];
+              }) cfg.letsencrypt.dnsChallenge.domains
+            );
           };
         };
         certificatesResolvers.letsencrypt.acme = lib.mkMerge [
@@ -66,9 +69,13 @@ in
           })
         ];
         api.dashboard = true;
+        accesslog = {
+          filePath = "/var/log/traefik/access.log";
+          # format = "json";
+        };
         log = {
           level = "INFO";
-          filePath = "/var/log/traefik.log";
+          filePath = "/var/log/traefik/traefik.log";
           format = "json";
         };
       };
@@ -98,12 +105,25 @@ in
             service = "prometheus-node-exporter";
             middlewares = [ "ip-allowlist" ];
           };
+          speedtest = {
+            entryPoints = [ "websecure" ];
+            rule = "Host(`speedtest.ingress.snyssen.be`)";
+            service = "speedtest";
+          };
         };
         http.services = {
           prometheus-node-exporter = {
             loadBalancer.servers = [
               { url = "http://localhost:9100"; }
             ];
+          };
+          speedtest = {
+            loadBalancer = {
+              servers = [
+                { url = "https://speedtest.snyssen.be"; }
+              ];
+              passHostHeader = false;
+            };
           };
         };
       };
