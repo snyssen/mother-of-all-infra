f0f7aaade9178aa5e87c53cdccb86a969622488c
snyssen <dev@snyssen.be>
2026-01-02 15:30:21 +0100

    fix(nix/modules/jellyfin): enable hardware transcoding on Intel CPU (QSV)

--- nix/modules/nixos/jellyfin.nix
+++ nix/modules/nixos/jellyfin.nix
@@ -1,10 +1,32 @@
-{ ... }:
+{ pkgs, ... }:
 {
   config = {
     services.jellyfin = {
       enable = true; # Will be available on http://localhost:8096/
       openFirewall = false; # This is for local use
       user = "snyssen";
+    };
+
+    systemd.services.jellyfin.environment.LIBVA_DRIVER_NAME = "iHD"; # or i965 for older GPUs
+    environment.sessionVariables = {
+      LIBVA_DRIVER_NAME = "iHD";
+    };
+
+    hardware.graphics = {
+      enable = true;
+
+      extraPackages = with pkgs; [
+        intel-ocl # Generic OpenCL support
+
+        # For Broadwell and newer (ca. 2014+), use with LIBVA_DRIVER_NAME=iHD:
+        intel-media-driver
+
+        # For 13th gen and newer:
+        intel-compute-runtime
+
+        # For 11th gen and newer:
+        vpl-gpu-rt
+      ];
     };
   };
 }
