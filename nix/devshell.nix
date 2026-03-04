e536d6a16daf77c2d4b9c32e973357de2b8cf200
snyssen <dev@snyssen.be>
2026-03-04 07:01:11 +0100

    feat(nix/devshell): automatically do a git pull when entering devshell
    I got tired of once more trying to deploy changes right after a PR, and actually break the server because I just ran old code because I forgot to pull the latest changes first...

--- nix/devshell.nix
+++ nix/devshell.nix
@@ -23,8 +23,11 @@ pkgs.mkShell {
 
   # Load custom bash code
   shellHook = ''
+    git pull
+    echo ""
     echo -e "This devshell uses '\e[32;1mjust\e[0m' as a task runner."
     just --list
     echo -e "\e[0;95mNOTE:\e[0m If this is your first time on this repo, running '\e[32;1mjust setup\e[0m' is recommended in order to initialize the development environment."
+    echo ""
   '';
 }
