8d4b58b0e654371c10c93a48f0edf837268db70d
snyssen <dev@snyssen.be>
2026-05-19 07:43:40 +0200

    fix(devshell): update git safety check

--- nix/devshell.nix
+++ nix/devshell.nix
@@ -23,11 +23,11 @@ pkgs.mkShell {
 
   # Load custom bash code
   shellHook = ''
-    bash scripts/git-main-safety-check.sh warn
     echo ""
     echo -e "This devshell uses '\e[32;1mjust\e[0m' as a task runner."
     just --list
     echo -e "\e[0;95mNOTE:\e[0m If this is your first time on this repo, running '\e[32;1mjust setup\e[0m' is recommended in order to initialize the development environment."
     echo ""
+    bash scripts/git-main-safety-check.sh warn
   '';
 }
