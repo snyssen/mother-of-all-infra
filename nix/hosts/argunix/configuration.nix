5c6fae8a4d6d0f4e5df1178a5df7cee2a32a19da
snyssen <dev@snyssen.be>
2026-07-23 08:23:36 +0200

    feat(nix/hosts/argunix): add VM for argunix

---
+++ nix/hosts/argunix/configuration.nix
@@ -0,0 +1,103 @@
+{
+  inputs,
+  flake,
+  config,
+  pkgs,
+  ...
+}:
+{
+  imports = [
+    flake.modules.nixos.disko
+    ./hardware-configuration.nix
+
+    flake.modules.nixos.sops
+    flake.modules.nixos.cache
+    flake.modules.nixos.grub
+    flake.modules.nixos.kbd-layout
+    flake.modules.nixos.shell
+    flake.modules.nixos.locale
+    flake.modules.nixos.nh
+
+    flake.modules.nixos.tailscale
+    # flake.modules.nixos.crowdsec-firewall-bouncer # TODO: generate API key and enable
+    flake.modules.nixos.prometheus-node-exporter
+    flake.modules.nixos.grafana-alloy
+    flake.modules.nixos.nfs-mounts
+  ];
+
+  disko =
+    let
+      ly = "single-btrfs-luks-virtiofs-key";
+    in
+    {
+      layout = ly;
+      "${ly}" = {
+        autoResizeOnBoot.enable = true;
+      };
+    };
+
+  sops.secrets = {
+    "tailscale/authKey" = {
+      sopsFile = ./data/secrets.yaml;
+    };
+    "users/snyssen/passwordHash" = {
+      sopsFile = ./data/secrets.yaml;
+      neededForUsers = true;
+    };
+  };
+
+  tailscale.autoconnect = {
+    enable = true;
+    authKeyPath = config.sops.secrets."tailscale/authKey".path;
+    enableSSH = true;
+  };
+
+  grafana-alloy = {
+    varlogs.enable = true;
+    journald.enable = true;
+  };
+
+  services.openssh = {
+    enable = true;
+    openFirewall = true;
+    settings.PasswordAuthentication = false;
+    settings.PermitRootLogin = "no";
+  };
+
+  users = {
+    mutableUsers = false;
+    users = {
+      snyssen = {
+        isNormalUser = true;
+        extraGroups = [
+          "networkmanager"
+          "wheel"
+        ];
+        hashedPasswordFile = config.sops.secrets."users/snyssen/passwordHash".path;
+        openssh.authorizedKeys.keys = [
+          "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIG68A6FS8yzwzaOUsoKHL9bc+2gB1P5OQriFjEWzG/LH snyssen@blackfog"
+        ];
+      };
+    };
+  };
+
+  nfsMounts.enable = false;
+  nfsMounts.mounts.bulk = {
+    path = "/mnt/bulk";
+    host = "hypervisor";
+    remotePath = "/mnt/bulk/argunix";
+    dependsOn.tailscale = true;
+  };
+
+  # TODO: make this part automatically defined
+  nix.settings = {
+    experimental-features = [
+      "nix-command"
+      "flakes"
+    ];
+    auto-optimise-store = true;
+  };
+  system.name = "argunix";
+  networking.hostName = "argunix";
+  system.stateVersion = "25.11";
+}
